04ea524346e71b2079f52927f58c719d0ff4dc6e
jivko25 <jivkou@yahoo.com>
2020-10-23 18:41:09 +0300

    new

--- src/SnakeMain.java
+++ src/SnakeMain.java
@@ -1,17 +1,20 @@
+import java.awt.Container;
+import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
 
 public class SnakeMain {
 	public static void main(String [] args) throws InterruptedException {
+		Scanner sc = new Scanner(System.in);
 		Borders px = new Borders('#',2,2);
-		px.Print();
-		for(int i = 10;i>=px.snakeX;i--) {
-		TimeUnit.SECONDS.sleep(1);
-		px.newPos(1,1);
-		px.Print();
+		String turn = sc.nextLine();
+		while(!px.isGameOver()) {
+		turn = sc.nextLine();
+		px.GameControlers(turn);
+		}
 		}
 	}
 	
-	public static class Borders{
+	class Borders{
 		char [][] px = new char[10][10];
 		int snakeX;
 		int snakeY;
@@ -21,53 +24,114 @@ public class SnakeMain {
 			for(int i = 0;i<px.length;i++) {
 				for(int k = 0;k<px.length;k++) {
 					if(i == 0 || i == px.length-1 || k == 0 || k == px.length-1) {
-						this.px[i][k] = symbol;
+						this.px[k][i] = symbol;
 					}
 					else if(i == this.snakeY && k == this.snakeY){
-						this.px[i][k] = 'o';
+						this.px[k][i] = 'o';
 					}
 					else {
-						this.px[i][k] = ' ';
+						this.px[k][i] = ' ';
 					}
 				}
 			}
+			Print();
 		}
 		
-		public void newPos(int x,int y) throws InterruptedException {
-			this.snakeX = this.snakeX + x;
-			this.snakeY = this.snakeY + y;
-			for(int i = 0;i<px.length;i++) {
-				for(int k = 0;k<px.length;k++) {
-					if(i == 0 || i == px.length-1 || k == 0 || k == px.length-1) {
-						this.px[i][k] = '#';
-					}
-					else if(i == this.snakeY && k == this.snakeY){
-						this.px[i][k] = 'o';
-					}
-					else {
-						this.px[i][k] = ' ';
-					}
-				}
-			}
+		public void TurnRight() throws InterruptedException {
+			this.px[this.snakeX][this.snakeY] = ' ';
+			this.snakeX = this.snakeX + 1;
+			this.px[this.snakeX][this.snakeY] = 'o';
+			Print();
+		}
+		
+		public void TurnLeft() throws InterruptedException {
+			this.px[this.snakeX][this.snakeY] = ' ';
+			this.snakeX = this.snakeX - 1;
+			this.px[this.snakeX][this.snakeY] = 'o';
+			Print();
+		}
+		
+		public void TurnUp() throws InterruptedException {
+			this.px[this.snakeX][this.snakeY] = ' ';
+			this.snakeY = this.snakeY - 1;
+			this.px[this.snakeX][this.snakeY] = 'o';
+			Print();
+		}
+		
+		public void TurnDown() throws InterruptedException {
+			this.px[this.snakeX][this.snakeY] = ' ';
+			this.snakeY = this.snakeY + 1;
+			this.px[this.snakeX][this.snakeY] = 'o';
+			Print();
 		}
 		
 		public void Print() {
 			for(int i = 0;i<px.length;i++) {
 				for(int k = 0;k<px.length;k++) {
-					System.out.print(this.px[i][k]);
+					System.out.print(this.px[k][i]);
 				}
 				System.out.println();
 			}
 		}
-		public void Delete() {
-			for(int i = 0;i<px.length;i++) {
-				for(int k = 0;k<px.length;k++) {
-					
-						this.px[i][k] = '\b';
-					
-				}
+		public boolean isGameOver() {
+			if(this.snakeX == 0 || this.snakeX == this.px.length-1 || this.snakeY == 0 || this.snakeY == this.px.length-1 ) {
+				return true;
 			}
-			Print();
+			else {
+				return false;
+			}
 		}
+		
+		public void GameControlers(String turn) throws InterruptedException {
+			boolean D = false;
+			boolean A = false;
+			boolean W = false;
+			boolean S = false;
+//			TimeUnit.SECONDS.sleep(1);
+			if(W == true) {
+				TurnUp();
+			}
+			else if(D == true) {
+				TurnRight();
+			}
+			else if(A == true) {
+				TurnLeft();
+			}
+			else if(S == true) {
+				TurnDown();
+			}
+			switch (turn) {
+			case "w":
+				TurnUp();
+				W = true;
+				A = false;
+				S = false;
+				D = false;
+				break;
+			case "s":
+				TurnDown();
+				W = false;
+				A = false;
+				S = true;
+				D = false;
+				break;
+			case "a":
+				TurnLeft();
+				W = false;
+				A = true;
+				S = false;
+				D = false;
+				break;
+			case "d":
+				TurnRight();
+				W = false;
+				A = false;
+				S = false;
+				D = true;
+				break;	
+			default:
+				break;
+			}
+		}
+		
 	}
-}
